526678a4acd7e01b0f79e7ea1beabb1ac966658e
Jenny Dcruz <jendcruz23@gmail.com>
2021-10-13 08:25:36 -0400

    modified: navigation and list

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function symptom_search() {
+
+    var input, filter, ul, li, a, i, txtValue;
+    
+    input = document.getElementById("userInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("listOfSymptoms");
+    li = ul.getElementsByTagName("li");
+    
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function showSymptom() {
+    document.getElementById('listOfSymptoms').style.display = "block";
+ }
+
+ function showSymptomDescription() {
+    document.getElementById('symptomDescription').style.display = "block";
+ }
