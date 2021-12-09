09ea49b078750a3b1657b7e12b03acf656d020af
Jenny Dcruz <jendcruz23@gmail.com>
2021-12-09 02:09:34 -0500

    modified: symptoms content

--- scripts.js
+++ scripts.js
@@ -1,30 +1,96 @@
+// SYMPTOM SEARCH FUNCTION TO SEARCH FOR THE VALUE THAT THE USER HAS ENTERED
 // Reference: https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
-
 function symptom_search() {
 
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, arrayOfSymptoms, i, txtValue;
     
+    // GET THE INPUT ENTERED BY THE USER IN THE SEARCH BAR
     input = document.getElementById("userInput");
+
+    // CONVERT USER INPUT TO LOWER CASE
     filter = input.value.toLowerCase();
-    ul = document.getElementById("listOfSymptoms");
-    li = ul.getElementsByTagName("li");
+
+    // THE arrayOfSymptoms VARIABLE STORES ALL THE LIST ELEMETS HAVE THE LI TAG
+    arrayOfSymptoms = document.getElementsByTagName("li");
     
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.innerText;
+    // LOOP TO CHECK IF THE INNER HTML FOR THE USER ENTERED TEXT EXISTS IN OUR LIST OF SYMPTOMS
+    for (i = 0; i < arrayOfSymptoms.length; i++) {
+        txtValue = arrayOfSymptoms[i].innerText;
+
+        // IF THE SUBSTRING EXISTS IN OUR LIST OF SYMPTOMS, THAT PARTICULAR SYMPTOM WILL BE DISPLAYED
         if (txtValue.toLowerCase().indexOf(filter) > -1) {
-            li[i].style.display = "block";
+            var elementID = i+1;
+            document.getElementById("symptom"+elementID).style.display = "block";
         } 
+
+        // IF THE SUBSTRING DOES NOT EXISTS IN OUR LIST OF SYMPTOMS, THAT PARTICULAR SYMPTOM WILL NOT BE DISPLAYED
         else {
-            li[i].style.display = "none";
+            arrayOfSymptoms[i].style.display = "none";
         }
     }    
 }
+ 
+// FUNCTION TO DISPLAY A PARTICULAR SYMPTOM'S DESCRIPTION
+function showSymptomDescription(id) {
+    document.getElementById('searchBox').style.display = "none";
+    document.getElementById('listOfSymptoms').style.display = "none";
+    document.getElementById('symptomDescription'+id).style.display = "block";
+    document.getElementById('searchButton').style.display = "block";
+}
 
-function showSymptom() {
-    document.getElementById('listOfSymptoms').style.display = "block";
- }
+jQuery(document).ready(function () {
+    $("#symptom1").click(function(){
+        showSymptomDescription(id=1);
+    })
 
- function showSymptomDescription() {
-    document.getElementById('symptomDescription').style.display = "block";
- }
+    $("#symptom2").click(function(){
+        showSymptomDescription(id=2);
+    })
+
+    $("#symptom3").click(function(){
+        showSymptomDescription(id=3);
+    })
+
+    $("#symptom4").click(function(){
+        showSymptomDescription(id=4);
+    })
+
+    $("#symptom5").click(function(){
+        showSymptomDescription(id=5);
+    })
+
+    $("#symptom6").click(function(){
+        showSymptomDescription(id=6);
+    })
+
+    $("#symptom7").click(function(){
+        showSymptomDescription(id=7);
+    })
+
+    $("#symptom8").click(function(){
+        showSymptomDescription(id=8);
+    })
+
+    $("#symptom9").click(function(){
+        showSymptomDescription(id=9);
+    })
+
+    $("#symptom10").click(function(){
+        showSymptomDescription(id=10);
+    })
+
+    $("#symptom11").click(function(){
+        showSymptomDescription(id=11);
+    })
+
+    $("#symptom12").click(function(){
+        showSymptomDescription(id=12);
+    })
+
+    $("#searchButton").click(function(){
+        document.getElementById('searchBox').style.display = "block";
+        document.getElementById('listOfSymptoms').style.display = "block";
+        document.getElementById('symptomDescription'+id).style.display = "none";
+        document.getElementById('searchButton').style.display = "none";
+    })
+});
